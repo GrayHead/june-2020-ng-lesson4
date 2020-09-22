28c04bd78f74a152bd4046108d6db722bb7543ef
GrayHead <ss.zhuravlov@gmail.com>
2020-09-22 21:12:17 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {RouterModule} from "@angular/router";
+import {AllUsersComponent} from './customComponents/all-users/all-users.component';
+import {HttpClientModule} from "@angular/common/http";
+import {UserComponent} from './customComponents/user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AllUsersComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'users', component: AllUsersComponent,
+        children: [
+          // users/posts/1
+          {path: 'posts/:id', component: UserComponent},
+
+
+        ]
+      },
+
+
+    ]),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/app/customComponents/all-users/all-users.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../services/user.service";
+
+@Component({
+  selector: 'app-all-users',
+  templateUrl: './all-users.component.html',
+  styleUrls: ['./all-users.component.css']
+})
+export class AllUsersComponent implements OnInit {
+
+  xxx: any[];
+
+  constructor(private userService: UserService) {
+
+    this.userService.allUsers().subscribe(users => {
+      this.xxx = users;
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
